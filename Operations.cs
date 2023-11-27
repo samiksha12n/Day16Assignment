fe5e3caa69ff48e349efc05d9761c86f867d2d60
samiksha12n <samikshaunesarkar@gmail.com>
2023-11-27 14:55:35 +0530

    Delegates

---
+++ Operations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16AssignmentPart1
+{
+    internal class Operations
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16AssignmentPart1
+{
+    internal class Program
+    {
+        internal delegate int ArithmeticOperation(int n1, int n2);
+        static void Main(string[] args)
+        {
+            Operations Opertations= new Operations();
+            Console.WriteLine("Choose the Operation");
+            Console.WriteLine("Enter first number");
+            int number1=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            int number2=int.Parse(Console.ReadLine());
+            Console.WriteLine("1.Addition 2.Substraction 3.Multiplication 4.Division");
+            int op=int.Parse(Console.ReadLine());
+            switch(op)
+            {
+                case 1:
+                    {
+                        ArithmeticOperation del = new ArithmeticException(Opertations.Add( number1,number2));
+                    }
+            }
+            
+        }
+    }
+}
